820a4dda4955b0313f0a40395f8544fc90c1bc93
pentwo <pentwo@gmail.com>
2022-01-28 10:45:01 +0800

    fix: remove Endpoint URL

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import './App.css'
 import Hero from './components/Hero'
 import List from './components/List'
 
-// const END_POINT:string|URL = new URL(process.env.REACT_APP_API_ENDPOINT)
-
 function App() {
   return (
     <div className="App">
