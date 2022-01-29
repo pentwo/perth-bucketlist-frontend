00bf446c66fe04204f1fe83353f729fd3a9f7963
pentwo <pentwo@gmail.com>
2022-01-30 06:18:52 +0800

    feat: added new font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    fontFamily: {
+      handwriting: ['Mali', 'cursive']
+    },
     extend: {}
   },
-  plugins: [require('tw-elements/dist/plugin')]
+  plugins: []
 }
