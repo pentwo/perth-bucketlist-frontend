6a329d714182e76d93761c23db926942cf1558c9
pentwo <pentwo@gmail.com>
2022-01-30 16:03:15 +0800

    working: modify figcaption style

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -25,13 +25,15 @@ const ItemCard = (props: Props) => {
         }}
       >
         <div className="absolute front p-4 h-full opacity-100 transition-all duration-1000 faceup border-2 border-gray-200 ">
-          <figure className="flex flex-col justify-center items-center text-gray-500">
+          <figure className="flex flex-col justify-center items-center text-gray-500 ">
             <img
               className="rounded-sm object-cover h-80 w-80"
               src={props.item.placeImg}
               alt="placeholder"
             />
-            <figcaption>{props.item.location}</figcaption>
+            <figcaption className="text-base font-handwriting p-1 -skew-x-6 -rotate-6 bg-indigo-200 -translate-y-3">
+              {props.item.location}
+            </figcaption>
           </figure>
         </div>
         <div className="absolute back p-4 h-full opacity-0 transition-all duration-1000 border-2 border-gray-200 overflow-y-auto">
@@ -53,7 +55,7 @@ const ItemCard = (props: Props) => {
               className="drop-shadow hover:text-indigo-700"
               width="36"
               height="36"
-              stroke-width="1.5"
+              strokeWidth="1.5"
               viewBox="0 0 24 24"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
@@ -61,21 +63,21 @@ const ItemCard = (props: Props) => {
               <path
                 d="M9 12H12M15 12H12M12 12V9M12 12V15"
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               />
               <path
                 d="M21 3.6V20.4C21 20.7314 20.7314 21 20.4 21H3.6C3.26863 21 3 20.7314 3 20.4V3.6C3 3.26863 3.26863 3 3.6 3H20.4C20.7314 3 21 3.26863 21 3.6Z"
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               />
             </svg>
             <svg
               className="hidden drop-shadow"
               width="36"
               height="36"
-              stroke-width="1.5"
+              strokeWidth="1.5"
               viewBox="0 0 24 24"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
@@ -83,14 +85,14 @@ const ItemCard = (props: Props) => {
               <path
                 d="M9.87871 14.1213L12 12M14.1213 9.87868L12 12M12 12L9.87871 9.87868M12 12L14.1213 14.1213"
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               />
               <path
                 d="M21 3.6V20.4C21 20.7314 20.7314 21 20.4 21H3.6C3.26863 21 3 20.7314 3 20.4V3.6C3 3.26863 3.26863 3 3.6 3H20.4C20.7314 3 21 3.26863 21 3.6Z"
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               />
             </svg>
           </div>
